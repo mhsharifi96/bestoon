ea38f738903b403fef13540f973edf2208316189
mh.sharifi96 <mh.sh7676@gmail.com>
2017-01-06 17:17:46 +0330

    expense models is second

--- web/models.py
+++ web/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 class Expense(models.Model):
     text=models.CharField(max_length=255)
-    date=models.DateField()
+    date=models.DateTimeField()
     amount=models.BigIntegerField()
     user=models.ForeignKey(User)
     
