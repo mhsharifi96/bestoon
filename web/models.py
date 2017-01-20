cee89ab4e6604db59562c6aa0ce361ab93a41eb2
mh.sharifi96 <mh.sh7676@gmail.com>
2017-01-20 17:17:20 +0330

    submit/expense and submit/income web services

--- web/models.py
+++ web/models.py
@@ -4,6 +4,12 @@ from django.db import models
 #import user 
 from django.contrib.auth.models import User
 # Create your models here.
+class Token(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    token=models.CharField(max_length=48)
+    def __unicode__(self):
+        return "{}-token".format(self.user)
+
 
 class Expense(models.Model):
     text=models.CharField(max_length=255)
